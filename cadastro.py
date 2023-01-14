5333bb04ee1331ba911d873d4154d82f230fe147
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-01-14 11:22:43 -0300

    cadastro
    Uma simples forma de fazer a Interface para o login no PySimpleGUI.

---
+++ cadastro.py
@@ -0,0 +1,22 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+# Layout
+
+sg.theme('Dark')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario', size=(20, 1))],
+    [sg.Text('Senha  '), sg.Input(key='senha', password_char='*', size=(20, 1))],
+    [sg.Text('Salvar Login?')],
+    [sg.Button('Entrar')]
+]
+# Janela
+janela = sg.Window('Tela de Login', layout)
+# Ler os eventos
+while True:
+    eventos, valores = janela.read()  # Untracking
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        valores['usuario'] == 'marcelo' and valores['senha'] == '12345'
+        print('Bem-vindo ao Serviço!')
+
